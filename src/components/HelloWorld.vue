3f382f2e4fb89041c90b5e979351f9aad2404dcb
jerry360 <jernej360@gmail.com>
2022-11-02 21:34:27 +0100

    PACK ( index.js && HelloWorld.vue ): added a store and simple breed fetch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,32 @@
 <template>
+  <div>
+    {{breeds}}
+  </div>
 </template>
 
 <script>
+import {mapActions, mapGetters} from "vuex";
 
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted () {
+    this.fetchBreeds
+  },
+  computed: {
+    ...mapGetters({
+      breeds: 'breeds'
+    }),
+    ...mapActions({
+      fetchBreeds: 'fetchBreeds'
+    }),
+  }
+}
 </script>
 
 <style scoped lang="scss">
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,46 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios  from "axios"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    breeds: {},
+    selectedBreed: null,
+    selectedDog: null,
+    nextDog: null,
+    previousDogs: [],
   },
   getters: {
+    breeds: state => state.breeds,
+    selectedBreed: state => state.selectedBreed,
+    selectedDog: state => state.selectedDog,
+    nextDog: state => state.nextDog,
+    previousDogs: state => state.previousDogs,
   },
   mutations: {
+    setBreeds(state, breeds) {
+      state.breeds = breeds
+    },
+    setSelectedBreed(state, breed) {
+      state.selectedBreed = breed
+    },
+    setSelectedDog(state, dog) {
+      state.selectedDog = dog
+    },
+    setNextDog(state, dog) {
+      state.nextDog = dog
+    },
+    setPreviousDogs(state, dogs) {
+      state.previousDogs = dogs
+    }
   },
   actions: {
+    async fetchBreeds({ commit }) {
+      const response = await axios.get('https://dog.ceo/api/breeds/list/all')
+      commit('setBreeds', response.data.message)
+    }
   },
   modules: {
   }
