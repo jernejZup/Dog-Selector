cb5dc883293f1c84f7d6f2f38c8d4f66135c91a3
jerry360 <jernej360@gmail.com>
2022-11-06 21:05:56 +0100

    FEAT ( HelloWorld.vue ): added dog selector

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,51 @@
 <template>
-  <div>
-    {{breeds}}
-  </div>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <template >
+          <v-data-table
+              :headers="headers"
+              :items="mainBreeds"
+              hide-default-footer
+              class="elevation-1"
+              disable-pagination
+              height="300"
+              :loading="mainBreeds.length === 0"
+              fixed-header
+              @click:row="useBreed"
+              :mobile-breakpoint="0"
+          >
+            <template v-slot:item.breed="{ item }">
+              {{item.breed}}
+              <v-icon
+                  v-if="item.subBreeds.length > 0"
+              >mdi-chevron-right</v-icon>
+            </template>
+          </v-data-table>
+        </template>
+      </v-col>
+      <v-col v-if="subBreeds.length>0">
+        <template >
+          <v-data-table
+              :headers="subBreedHeaders"
+              :items="subBreeds"
+              hide-default-footer
+              class="elevation-1"
+              disable-pagination
+              height="300"
+              :loading="mainBreeds.length === 0"
+              fixed-header
+              @click:row="useSubBreed"
+              :mobile-breakpoint="0"
+          >
+            <template v-slot:item.breed="{ item }">
+              {{item.breed}}
+            </template>
+          </v-data-table>
+        </template>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -11,21 +55,54 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      subBreeds:[],
+      headers: [
+        {
+          text: 'Dog Breed',
+          align: 'start',
+          sortable: false,
+          value: 'breed',
+        },
+      ],
+      subBreedHeaders: [
+        {
+          text: 'Optional sub Breed',
+          align: 'start',
+          sortable: false,
+          value: 'breed',
+        },
+      ]
     }
   },
   methods: {
+    ...mapActions({
+      fetchBreeds: 'fetchBreeds',
+      setSelectedBreed: "setSelectedBreed"
+    }),
+    useBreed(item, row) {
+      this.setSelectedBreed(item.breed)
+      this.subBreeds = item.subBreeds.map(item => ({ breed: item }));
+    },
+    useSubBreed(item, row) {
+      if(this.selectedBreed.includes('/')){
+        this.setSelectedBreed(this.selectedBreed.split('/')[0] + '/' + item.breed)
+      }else {
+        this.setSelectedBreed(this.selectedBreed + '/' + item.breed)
+      }
+    }
   },
   mounted () {
-    this.fetchBreeds
+    this.fetchBreeds()
   },
   computed: {
     ...mapGetters({
-      breeds: 'breeds'
+      breeds: 'breeds',
+      selectedBreed: 'selectedBreed'
     }),
-    ...mapActions({
-      fetchBreeds: 'fetchBreeds'
-    }),
-  }
+    mainBreeds() {
+      return Object.keys(this.breeds).map(item => ({ breed: item, subBreeds: this.breeds[item] || [] }));
+    },
+  },
 }
 </script>
 
