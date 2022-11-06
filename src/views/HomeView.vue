8f032682b028feb4a955ff139f4776e7db25111e
jerry360 <jernej360@gmail.com>
2022-11-06 21:04:49 +0100

    FEAT ( HomeView.vue ): added default view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,105 @@
 <template>
-  <div class="home">
-    <HelloWorld/>
-  </div>
+  <v-card class="overflow-hidden">
+    <!-- APP TOOLBAR -->
+    <v-app-bar
+      absolute
+      color="#6A76AB"
+      dark
+      shrink-on-scroll
+      fade-img-on-scroll
+      :src="randomDogImageLink"
+      scroll-target="#scrolling-techniques-2"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-app-bar-title class="title">DOG PICKER</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <template v-slot:extension>
+        <v-tabs v-model="tab">
+          <v-tab v-for="tabName in tabs" :key="tabName">
+            {{ tabName }}
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+
+    <!-- APP Container -->
+    <v-sheet
+      id="scrolling-techniques-2"
+      class="overflow-y-auto"
+      max-height="100%"
+      style="height: 100vh"
+    >
+      <v-container fluid style="margin-top: 250px">
+        <by-breed v-if="tab === 0" />
+        <random-dog v-if="tab === 1" />
+      </v-container>
+    </v-sheet>
+
+    <!-- APP DRAWER -->
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Dog picker</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item @click="$router.push('/about')">
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ByBreed from "@/components/ByBreed";
+import { mapActions } from "vuex";
+import RandomDog from "@/components/RandomDog";
 
 export default {
-  name: 'HomeView',
+  name: "Home",
+  data() {
+    return {
+      randomDogImageLink: "",
+      drawer: false,
+      group: null,
+      tab: null,
+      tabs: ["By Breed", "Random"],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    RandomDog,
+    ByBreed,
+  },
+  methods: {
+    ...mapActions({
+      fetchRandomDogImage: "fetchRandomDogImage",
+    }),
+  },
+  mounted() {
+    this.fetchRandomDogImage().then((response) => {
+      this.randomDogImageLink = response.data.message;
+    });
+  },
+};
 </script>
