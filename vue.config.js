d33b3d8a712bfe705e25f900d890b8e9120753a6
jerry360 <jernej360@gmail.com>
2022-11-06 20:30:53 +0100

    PACK ( App.vue && index.html && main.js && package.json && package-lock.json && vue.config.js && vuetify.js ): added vuetify package

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new VuetifyLoaderPlugin()
+    ],
+  },
+}
