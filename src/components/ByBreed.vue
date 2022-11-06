9729bf697480fd3da08965b4ee2e4d81f6202b35
jerry360 <jernej360@gmail.com>
2022-11-06 21:07:13 +0100

    FEAT ( DogGallery.vue ): wrapped gallery and selector in to single component

---
+++ src/components/ByBreed.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <hello-world/>
+    <dog-gallery/>
+  </div>
+</template>
+
+<script>
+import DogGallery from "./DogGallery";
+import HelloWorld from "./HelloWorld";
+export default {
+  name: "ByBreed",
+  components: {
+    DogGallery,
+    HelloWorld,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
